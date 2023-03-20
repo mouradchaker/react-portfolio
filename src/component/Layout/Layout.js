45b77298abdaedd2f3218e6f3689dc29dde1a605
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-03-20 23:23:05 +0100

    layout

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar/Sidebar'
 import './Layout.scss'
 
 function Layout() {
   return (
-    <div>
+    <div className="App">
       <Sidebar />
+
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
     </div>
   )
 }
