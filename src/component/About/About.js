7b5866b490b9faf71c8949bb2253e7d351da80de
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-03-23 00:56:04 +0100

    About

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import './About.scss'
 
 function About() {
-  return <div>About</div>
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+            idx={15}
+          />
+        </h1>
+        <p>
+          I'm a very ambitious front-end developer looking for a role in an
+          established IT company with the opportunity to work with the latest
+          technologies on challenging and diverse projects.
+        </p>
+        <p>
+          I'm quiet confident, naturally curious, and perpetually working on
+          improving my chops one design problem at a time.
+        </p>
+        <p>
+          If I need to define myself in one sentence that would be a family
+          person, father of a beautiful daughter, a sports fanatic, photography
+          enthusiast, and tech-obsessed!!!
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default About
