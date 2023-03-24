a3dfdec486e9381a386bb4e4fff1e2715637cb41
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-03-25 00:09:57 +0100

    contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 import About from './component/About/About'
+import Contact from './component/Contact/Contact'
 import Home from './component/Home/Home'
 import Layout from './component/Layout/Layout'
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
