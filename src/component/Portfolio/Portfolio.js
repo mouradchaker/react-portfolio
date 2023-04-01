fb2875344090a981c295e374d2b6ab80bc072fcb
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-04-01 18:01:34 +0100

    myWorkSection

--- src/component/Portfolio/Portfolio.js
+++ src/component/Portfolio/Portfolio.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import './Portfolio.scss'
 
 function Portfolio() {
-  return <div>Portfolio</div>
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['M', 'y', ' ', 'W', 'o', 'r', 'k']}
+            idx={15}
+          />
+        </h1>
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Portfolio
