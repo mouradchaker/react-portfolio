7289f20c6941c1b794ace0827d7e2f43b078cf3c
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-04-03 01:22:59 +0100

    mywork-content

--- src/component/Portfolio/Portfolio.js
+++ src/component/Portfolio/Portfolio.js
@@ -24,6 +24,14 @@ function Portfolio() {
                 className="portfolio-image"
                 alt="portfolio"
               />
+
+              <div className="content">
+                <p className="title">{port.title}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  View
+                </button>
+              </div>
             </div>
           )
         })}
