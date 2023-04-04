af110801604a1bac0ca7c825afd062ef44650b14
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-04-04 01:10:00 +0100

    skills-pacman

--- src/component/Skills/Skills.js
+++ src/component/Skills/Skills.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import SphereCanvas from '../SphereCanvas'
 import './Skill.scss'
@@ -13,65 +14,71 @@ const Skills = () => {
   }, [])
 
   return (
-    <div className="container skills-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['Skills', ' ', '&', ' ', 'Experience']}
-            idx={15}
-          />
-        </h1>
+    <>
+      <div className="container skills-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['Skills', ' ', '&', ' ', 'Experience']}
+              idx={15}
+            />
+          </h1>
 
-        <p>
-          Expert in front-end development including technologies like
-          <span class="tech-tag">HTML5</span>,<span class="tech-tag">CSS3</span>
-          ,<span class="tech-tag">JavaScript</span>,
-          <span class="tech-tag">jQuery</span>,
-          <span class="tech-tag">Angular2</span>,
-          <span class="tech-tag">React</span>,
-          <span class="tech-tag">TypeScript</span>,
-          <span class="tech-tag">Jasmine</span>,
-          <span class="tech-tag">Bootstrap</span>,
-          <span class="tech-tag">Sass</span>,<span class="tech-tag">Git</span>,
-          etc.
-        </p>
+          <p>
+            Expert in front-end development including technologies like
+            <span class="tech-tag">HTML5</span>,
+            <span class="tech-tag">CSS3</span>,
+            <span class="tech-tag">JavaScript</span>,
+            <span class="tech-tag">jQuery</span>,
+            <span class="tech-tag">Angular2</span>,
+            <span class="tech-tag">React</span>,
+            <span class="tech-tag">TypeScript</span>,
+            <span class="tech-tag">Jasmine</span>,
+            <span class="tech-tag">Bootstrap</span>,
+            <span class="tech-tag">Sass</span>,<span class="tech-tag">Git</span>
+            , etc.
+          </p>
 
-        <p>
-          I’m not a designer but I have a good sense of aesthetics, and
-          experience in responsive, mobile-first web design. I put special
-          effort into optimizing my code and providing the best user experience.
-          I would love to give you any kind of support also after the project's
-          completion. I guarantee a commitment during work on your project.
-        </p>
+          <p>
+            I’m not a designer but I have a good sense of aesthetics, and
+            experience in responsive, mobile-first web design. I put special
+            effort into optimizing my code and providing the best user
+            experience. I would love to give you any kind of support also after
+            the project's completion. I guarantee a commitment during work on
+            your project.
+          </p>
 
-        <p>
-          Visit my
-          <a target="_blank" href="https://www.linkedin.com/in/chmourad/">
-            LinkedIn{' '}
-          </a>
-          profile for more details. Also you can checkout my cv on this
-          <a
-            target="_blank"
-            href="https://bobangajicsm.github.io/portfolio/assets/slobodan-gajic-cv.pdf"
-          >
-            link
-          </a>
-          , or feel free to give a peek on some of my blog
-          <a
-            target="_blank"
-            href="https://www.toptal.com/css/smacss-scalable-modular-architecture-css"
-          >
-            posts
-          </a>
-          .
-        </p>
+          <p>
+            Visit my
+            <a target="_blank" href="https://www.linkedin.com/in/chmourad/">
+              LinkedIn{' '}
+            </a>
+            profile for more details. Also you can checkout my cv on this
+            <a
+              target="_blank"
+              href="https://bobangajicsm.github.io/portfolio/assets/slobodan-gajic-cv.pdf"
+            >
+              link
+            </a>
+            , or feel free to give a peek on some of my blog
+            <a
+              target="_blank"
+              href="https://www.toptal.com/css/smacss-scalable-modular-architecture-css"
+            >
+              posts
+            </a>
+            .
+          </p>
+        </div>
+
+        <div className="skills-charts">
+          <SphereCanvas />
+        </div>
       </div>
 
-      <div className="skills-charts">
-        <SphereCanvas />
-      </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
