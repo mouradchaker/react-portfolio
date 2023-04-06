aa05e30e40dae06869650be3b891c05de69a7341
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-04-06 14:59:19 +0100

    fixed a bug

--- src/component/Home/Logo/Logo.js
+++ src/component/Home/Logo/Logo.js
@@ -1,43 +1,12 @@
 import React, { useEffect, useRef } from 'react'
 import './Logo.scss'
 import LogoS from '../../../assets/images/logo-s.png'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+
 import profile from '../../../assets/images/profil.png'
 import profile2 from '../../../assets/images/profil2.jpeg'
 function Logo() {
-  const bgRef = useRef()
-  const outlineLogoRef = useRef()
-  const solidLogoRef = useRef()
-
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      })
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    )
-  }, [])
-
   return (
-    <div className="logo-container" ref={bgRef}>
+    <div className="logo-container">
       <div id="frame">
         <div id="border">
           <img src={profile2} alt="" />
